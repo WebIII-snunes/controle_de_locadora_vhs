6d9180ddbbdf41c5f5ea52e66de444c194c37a67
SNunesA <snunesadiers@gmail.com>
2025-07-07 19:24:57 -0300

    init:

---
+++ locadoravhs/src/main/java/com/example/locadoravhs/LocadoravhsApplication.java
@@ -0,0 +1,13 @@
+package com.example.locadoravhs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocadoravhsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocadoravhsApplication.class, args);
+	}
+
+}
---
+++ locadoravhs/src/test/java/com/example/locadoravhs/LocadoravhsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.locadoravhs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocadoravhsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
