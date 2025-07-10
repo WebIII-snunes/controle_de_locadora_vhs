b85c32e85786d1d5e9d2603a9794b178e4d2b68b
SNunesA <snunesadiers@gmail.com>
2025-07-10 17:10:08 -0300

    feat: categoria

--- locadoravhs/src/main/java/com/example/locadoravhs/models/Category.java
+++ locadoravhs/src/main/java/com/example/locadoravhs/models/Category.java
@@ -1,5 +1,30 @@
 package com.example.locadoravhs.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
