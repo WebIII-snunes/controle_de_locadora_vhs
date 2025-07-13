be705299d2fc43c902344fdda44eac881cf53901
SNunesA <snunesadiers@gmail.com>
2025-07-13 16:59:07 -0300

    feat: verificar a existencia de uma categoria

--- locadoravhs/src/main/java/com/example/locadoravhs/repositories/VHSRepository.java
+++ locadoravhs/src/main/java/com/example/locadoravhs/repositories/VHSRepository.java
@@ -3,7 +3,10 @@ package com.example.locadoravhs.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.example.locadoravhs.models.Category;
 import com.example.locadoravhs.models.VHS;
 
 @Repository
-public interface VHSRepository extends JpaRepository<VHS, Long> {}
+public interface VHSRepository extends JpaRepository<VHS, Long> {
+    boolean existsByCategory(Category category);
+}
