b28b1b07e2053915d0ed4bd5d491a3a00da212d4
SNunesA <snunesadiers@gmail.com>
2025-07-12 23:23:55 -0300

    fix: adicionei o lombok e uma função para representaçao

--- locadoravhs/src/main/java/com/example/locadoravhs/models/Category.java
+++ locadoravhs/src/main/java/com/example/locadoravhs/models/Category.java
@@ -4,7 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 @Entity
+@Data
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -12,19 +14,9 @@ public class Category {
 
     private String name;
 
-    public Long getId() {
-        return id;
+    @Override
+    public String toString() {
+        return this.name;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
