b9e24b05149e1251e960480901ea9c6b9b26bb8b
SNunesA <snunesadiers@gmail.com>
2025-07-13 16:58:47 -0300

    refactor: apaguei imports desnecessarios

--- locadoravhs/src/main/java/com/example/locadoravhs/models/User.java
+++ locadoravhs/src/main/java/com/example/locadoravhs/models/User.java
@@ -1,16 +1,12 @@
 package com.example.locadoravhs.models;
 
-import java.util.ArrayList;
-import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.Data;
-import lombok.ToString;
 
 @Entity 
 @Data 
@@ -25,7 +21,5 @@ public class User {
 
     private String senha;
 
-    // @OneToMany(mappedBy = "usuario")
-    // @ToString.Exclude
     
 }
